b43b4221cd461b7d135977f6189bb90d0b168cd1
petrusz2002 <petreusz2002@gmail.com>
2026-05-29 03:27:57 -0400

    db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:C5iWKrfOwdXri4Q0BGJa6jT5@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
